8f382c71389745c05ba7fad3baf555ac14a0f13a
zhaoyunchong <m15036570364@163.com>
2021-02-19 16:25:37 +0800

    keyi

--- timeFormat.js
+++ timeFormat.js
@@ -1,6 +1,7 @@
 // 秒转换分钟00:00:00格式
 function timeToMinute(times){
     console.log('赵云冲的git')
+    console.log('hello world');
        var t;
         if(times > -1){
             var hour = Math.floor(times/3600);
